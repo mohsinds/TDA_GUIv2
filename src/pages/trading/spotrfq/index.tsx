25f3fa0dfa929ccb136e6edceea09d67c191e71d
Ubaid Rehman <147931483+ubaidurrehmann@users.noreply.github.com>
2024-03-01 21:23:14 +0500

    tile changeas

--- src/pages/trading/spotrfq/index.tsx
+++ src/pages/trading/spotrfq/index.tsx
@@ -1,5 +1,59 @@
-import PageTitle from "@/components/TextDisplay/PageTitle"
+import PageTitle from "@/components/TextDisplay/PageTitle";
+import SubTitle from "@/components/TextDisplay/SubTitle";
+import Tile from "@/components/RFQ Tile/Tile";   
+import { Box, CardMedia, IconButton, SvgIcon, Paper } from "@mui/material";
+// import Image from 'mui-image'
+// import Hlogo from "../../../Images/Logo_dark.jpg"
+// import HLOGO from "@/Images/dark.png"
+import HLOGO from "../../../../src/Images/dark.png"
+// import Image from 'material-ui-image'
 
+// import { ReactComponent as MySvg } from "../../../Images/ec_logo.svg"; 
 export default function SpotRFQPage() {
-  return <PageTitle sx={{ textWrap: "nowrap", minWidth: "300px" }}>Spot RFQ</PageTitle>
+  // const imagin = () => {
+  //   // return <Image src="../../../Images/dark.png"/>
+  // }
+  return (
+    <>
+      <PageTitle
+        sx={{
+          textWrap: "nowrap",
+          minWidth: "300px",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        Request for Quote Portal
+      </PageTitle>
+      <SubTitle
+        sx={{
+          textWrap: "nowrap",
+          minWidth: "300px",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        Click Initiate RFQ to intantly receive a quote
+      </SubTitle>
+      <Box
+      sx={{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:3,
+      }}
+      >
+      <Tile />
+      </Box>
+      <Box>
+      {/* <Image src="https://app.condorcash.com/Logo.png"/> */}
+      </Box>
+      {/* <div>
+        <img src="../../../Images/ug.jpeg"/>
+      </div> */}
+    </>
+  );
 }
+// Click Initiate RFQ to intantly receive a quote 
